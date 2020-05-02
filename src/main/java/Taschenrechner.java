6e6ab77528c7ccf2ba6e0cb4aa6a0fdc795b376f
BenediktOhler <benediktohler@gmx.de>
2020-05-02 11:58:59 +0200

    Test

---
+++ src/main/java/Taschenrechner.java
@@ -0,0 +1,38 @@
+public class Taschenrechner {
+
+    public int int_Addition(int summand1,int summand2) {
+        return summand1 + summand2;
+    }
+
+    public float float_Addition(float summand1,float summand2){
+        return summand1 + summand2;
+    }
+
+    public int int_Subtraktion(int minuend,int subtrahend){
+        return minuend - subtrahend;
+    }
+
+    public float float_Subtraktion(float minuend,float subtrahend){
+        return minuend - subtrahend;
+    }
+
+    public int int_Multiplikation(int multiplikand,int multiplikator) {
+        return multiplikand * multiplikator;
+    }
+
+    public float float_Multiplikation(float multiplikand,float multiplikator) {
+        return multiplikand * multiplikator;
+    }
+
+    public int int_Division(int dividend,int divisor) {
+        return dividend / divisor;
+    }
+
+    public float float_Division(float dividend,float divisor) {
+        return dividend / divisor;
+    }
+
+
+
+
+}
---
+++ src/test/java/TaschenrechnerTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatches.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaschenrechnerTest {
+    int int_Zahl1 = 1;
+    int int_Zahl2 = 4;
+
+    @Test
+    void int_Addition() {
+
+    }
+     /*
+    @Test
+    void float_Addition() {
+    }
+
+    @Test
+    void int_Subtraktion() {
+    }
+
+    @Test
+    void float_Subtraktion() {
+    }
+
+    @Test
+    void int_Multiplikation() {
+    }
+
+    @Test
+    void float_Multiplikation() {
+    }
+
+    @Test
+    void int_Division() {
+    }
+
+    @Test
+    void float_Division() {
+    }
+    */
+
+}
